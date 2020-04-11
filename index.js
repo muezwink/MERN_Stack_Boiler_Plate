ff91d4d26cad159f97d351f036d0981c2e0760d0
System Administrator <root@jsonmraz.local>
2020-04-11 12:08:36 +0900

    made login routing

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const cookieParser = require('cookie-parser');
 const config = require('./config/key')
 
 const { User } = require('./models/user');
-mongoose.connect(''
-                , {useNewUrlParser: true}).then(() => console.log('DB connected'))
-                                          .catch(err => console.error(err)
+mongoose.connect(config.mongoURI, {useNewUrlParser: true})
+        .then(() => console.log('DB connected'))
+        .catch(err => console.error(err)
                 );
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -30,5 +30,34 @@ app.post('/api/users/register', (req, res) => {
         })
     })
 })
+
+app.post("/api/user/login", (req, res) => {
+    //find the email
+    User.findOne({ email: req.body.email }, (err, user) => {
+      if (!user)
+        return res.json({
+          loginSuccess: false,
+          message: "Auth failed, email not found"
+        });
+      //comparePassword
+  
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (!isMatch) {
+          return res.json({ loginSuccess: false, message: "wrong password" });
+        }
+      });
+  
+      //generateToken
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({
+            loginSuccess: true
+          });
+      });
+    });
+  });
 
 app.listen(4000);
--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -28,6 +31,42 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+userSchema.pre('save', function( next ) {
+    var user = this;
+    
+    if(user.isModified('password')){
+
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err);
+    
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash 
+            })
+        })
+    } else {
+        next ()
+    }
+});
+
+userSchema.methods.comparePassword = function(plainPassword,cb){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if (err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    var token =  jwt.sign(user._id.toHexString(),'secret')
+
+    user.token = token;
+    user.save(function (err, user){
+        if(err) return cb(err)
+        cb(null, user);
+    })
+}
 
 const User = mongoose.model('User', userSchema);
 
